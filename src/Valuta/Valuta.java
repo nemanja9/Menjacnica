68d843b2277aef12af4738b43f1dc434d48634c4
nemanjajurisic <nemanjajurisic@192.168.0.11>
2018-03-15 21:29:22 +0100

    Dodate metode toString, hashCode i equals

--- src/Valuta/Valuta.java
+++ src/Valuta/Valuta.java
@@ -1,4 +1,5 @@
 package Valuta;
+
 import java.util.GregorianCalendar;
 
 public class Valuta {
@@ -56,4 +57,62 @@ public class Valuta {
 	public void setSrednjiKurs(double srednjiKurs) {
 		this.srednjiKurs = srednjiKurs;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceni == null) ? 0 : skraceni.hashCode());
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (skraceni == null) {
+			if (other.skraceni != null)
+				return false;
+		} else if (!skraceni.equals(other.skraceni))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceni=" + skraceni + ", datum=" + datum + ", kupovniKurs=" + kupovniKurs
+				+ ", prodajniKurs=" + prodajniKurs + ", srednjiKurs=" + srednjiKurs + "]";
+	}
+	
+
 }
