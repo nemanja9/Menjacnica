197bbcfb7521c4ff8ec923ba28402e0ea65c4f84
nemanjajurisic <nemanjajurisic@192.168.0.11>
2018-03-15 22:17:58 +0100

    Implmentirane metode za operacije sa kursevima iz interfejsa

--- src/MenjanjeKursa.java
+++ src/MenjanjeKursa.java
@@ -1,24 +1,30 @@
 import java.util.GregorianCalendar;
 
+import Valuta.Valuta;
 import interfejs.MenjacnicaInterfejs;
 
 public class MenjanjeKursa implements  MenjacnicaInterfejs{
 
 	@Override
 	public void dodajKursValute(GregorianCalendar dan, double kurs) {
-		// TODO Auto-generated method stub
+		Valuta valuta = new Valuta();
+		valuta.setDatum(dan);
+		valuta.setKupovniKurs(kurs);
 		
 	}
 
 	@Override
 	public void obrisiKursValute(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		Valuta valuta = new Valuta();
+		valuta.setKupovniKurs(0);
 		
 	}
 
 	@Override
 	public double vratiKursZaNekiDan(GregorianCalendar dan) {
-		// TODO Auto-generated method stub
+		Valuta valuta = new Valuta();
+		if (valuta.getDatum().equals(dan))
+			return valuta.getKupovniKurs();
 		return 0;
 	}
 
