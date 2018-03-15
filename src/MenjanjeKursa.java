bc8833e611dcb0c1efd38fa4c2dd7d1dc217f1fd
nemanjajurisic <nemanjajurisic@192.168.0.11>
2018-03-15 22:05:29 +0100

    Dodata nova klasa sa implementiranim nefunkcionalnim metodama interfejsa

---
+++ src/MenjanjeKursa.java
@@ -0,0 +1,25 @@
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class MenjanjeKursa implements  MenjacnicaInterfejs{
+
+	@Override
+	public void dodajKursValute(GregorianCalendar dan, double kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKursValute(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double vratiKursZaNekiDan(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
